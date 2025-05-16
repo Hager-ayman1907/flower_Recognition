b5bd40a9dd99410078f2d1b83ad5417fd45bc2ea
hager <hager@@gmail.com>
2025-05-16 22:23:25 +0300

    Fix tensorflow import and update requirements

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 #import keras
 from tensorflow import keras
-from keras.models import load_model
+from tensorflow.keras.models import load_model 
 import streamlit as st 
 import tensorflow as tf
 import numpy as np
